924ff6a40e73b5640cdb08117bc4d2268ff902a5
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2023-02-22 12:31:35 +0100

    Añadido SingleSession al proyecto.

--- src/main/java/main/SingleSession.java
+++ src/main/java/main/SingleSession.java
@@ -4,10 +4,44 @@
  */
 package main;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  *
  * @author FPShare
  */
 public class SingleSession {
-    
+    public static SingleSession instance = null;
+    public Session session;
+
+
+    /***
+     * Declaramos los Atributos de la clase SingleSession, que seran los objetos Session y SessionFactory.
+     */
+    public SingleSession() {
+        SessionFactory sessionFactory = new Configuration().configure("hibernateConfig/hibernate.cfg.xml").buildSessionFactory();
+        session = sessionFactory.openSession();
+    }
+    /***
+     * Funcion que genera una nueva Instancia de SingleSession.
+     * @return 
+     */
+    public static SingleSession getInstance() {
+        if (instance == null) {
+            instance = new SingleSession();
+        }
+        return instance;
+    }
+
+    /***
+     * A travès del metodo getSession, podemos devolver una session abierta en vez de Instanciar otra session, 
+     * Al no haber dos sessiones abiertas, tendremos menos errores al hacer el RollBack.
+     * 
+     * @return 
+     */
+    public Session getSessio() {
+        return session;
+    }
 }
